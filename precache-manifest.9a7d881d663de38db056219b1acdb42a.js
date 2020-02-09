23fc4c9e3e31f78dea9994c851356ec93e9135ac
Daria Usatyuk <dashok.myr@gmail.com>
2020-02-08 16:19:07 -0800

    Updates

---
+++ precache-manifest.9a7d881d663de38db056219b1acdb42a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8c8fa6c1606b6b80f9ca8c014c542e1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "99f8e00214244fb14122",
+    "url": "/monsters-rolodex/static/css/main.8291b1f0.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "99f8e00214244fb14122",
+    "url": "/monsters-rolodex/static/js/main.07fe980d.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
